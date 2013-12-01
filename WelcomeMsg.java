1f28601599b819070951d847030f00dc566f2547
Andreas Hernandez <andreashdez@gmail.com>
2013-12-01 19:58:25 +0000

    Updated indentation

--- WelcomeMsg.java
+++ WelcomeMsg.java
@@ -1,10 +1,17 @@
 public class WelcomeMsg {
+
 	public static void main(String[] args) {
+
 		if (args.length == 0) {
 			System.out.println(" [ERROR] No user specified.");
-		} else if (args[0].equals("james")) {
-			User user = new User("James");
+
+		} else if (args[0].equals("andreas")) {
+			User user = new User("Andreas");
 			user.welcomeMsg();
+
+		} else if (args[0].equals("jess")) {
+			User user = new User("Jess");
+
 		} else {
 			System.out.println(" [ERROR] User "+args[0]+" does not exist.");
 		}
